6b8bb54d60c01028c8671ef03d5e8c9085cde438
robbyronk <robby.ronk@gmail.com>
2016-06-04 21:42:08 +0000

    reformat

--- src/containers/page-income.js
+++ src/containers/page-income.js
@@ -1,46 +1,44 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-
-import Table from '../components/table'
+import React, {Component} from "react";
+import {connect} from "react-redux";
 
 class PageIncome extends Component {
 
-    render() {
-        return (
-            <div>
-                <h3>Income for 1/1/2016 - 8/1/2016</h3>
-                <button className="button expanded">Add an Income</button>
-                <table>
-                    <thead>
-                    <tr>
-                        <td>Date</td>
-                        <td>Who</td>
-                        <td>Amount</td>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1/1/2016</td>
-                        <td>Acme Co</td>
-                        <td>$500</td>
-                    </tr>
-                    </tbody>
-                    <tfoot>
-                    <tr>
-                        <td>Total</td>
-                        <td>&nbsp;</td>
-                        <td>$500</td>
-                    </tr>
-                    </tfoot>
-                </table>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h3>Income for 1/1/2016 - 8/1/2016</h3>
+        <button className="button expanded">Add an Income</button>
+        <table>
+          <thead>
+          <tr>
+            <td>Date</td>
+            <td>Who</td>
+            <td>Amount</td>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>1/1/2016</td>
+            <td>Acme Co</td>
+            <td>$500</td>
+          </tr>
+          </tbody>
+          <tfoot>
+          <tr>
+            <td>Total</td>
+            <td>&nbsp;</td>
+            <td>$500</td>
+          </tr>
+          </tfoot>
+        </table>
+      </div>
+    )
+  }
 
 }
 
 function mapStateToProps(state) {
-    return {}
+  return {}
 }
 
 export default connect(mapStateToProps, {})(PageIncome)
