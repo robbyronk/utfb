113c7ce0863af7d4b2d20d088a53dc5587e183e5
robbyronk <robby.ronk@gmail.com>
2016-06-04 21:56:04 +0000

    Sketch payday page

--- src/containers/page-income.js
+++ src/containers/page-income.js
@@ -2,12 +2,21 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 
 class PageIncome extends Component {
+  // todo datepicker
 
   render() {
+    const deleteSwitchStyle = {
+      'display': 'flex',
+      'flexDirection': 'row',
+      'justifyContent': 'space-around',
+      'alignItems': 'center'
+    }
+
     return (
       <div>
-        <h3>Income for 1/1/2016 - 8/1/2016</h3>
+        <h3>Paydays for 1/1/2016 - 8/1/2016</h3>
         <button className="button expanded">Add an Income</button>
+        <button className="alert hollow button expanded">Delete Incomes</button>
         <table>
           <thead>
           <tr>
@@ -18,9 +27,53 @@ class PageIncome extends Component {
           </thead>
           <tbody>
           <tr>
+            <td>
+              <input defaultValue="1/1/2016"
+                     type="text"/>
+            </td>
+            <td>
+              <input defaultValue="Acme Co"
+                     type="text"/>
+            </td>
+            <td>
+              <input defaultValue="100"
+                     type="text"/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <input defaultValue="1/1/2016"
+                     type="text"/>
+            </td>
+            <td>
+              <input defaultValue="Acme Co"
+                     type="text"/>
+            </td>
+            <td>
+              <input defaultValue="200"
+                     type="text"/>
+            </td>
+          </tr>
+          <tr>
             <td>1/1/2016</td>
             <td>Acme Co</td>
-            <td>$500</td>
+            <td>
+              <div style={deleteSwitchStyle}>
+                <div>$200</div>
+                <div className="switch tiny">
+                  <input className="switch-input"
+                         id="tinySwitch"
+                         name="exampleSwitch"
+                         type="checkbox"/>
+                  <label className="switch-paddle"
+                         htmlFor="tinySwitch">
+                                <span className="show-for-sr">
+                                  Deleting
+                                </span>
+                  </label>
+                </div>
+              </div>
+            </td>
           </tr>
           </tbody>
           <tfoot>
@@ -31,6 +84,7 @@ class PageIncome extends Component {
           </tr>
           </tfoot>
         </table>
+        <button className="success button expanded">Finish</button>
       </div>
     )
   }
