219d835cd6cdde5ceb0c8518253844f93669c3ba
robbyronk <robby.ronk@gmail.com>
2016-06-08 21:33:25 +0000

    reformat

--- src/containers/page-budget.js
+++ src/containers/page-budget.js
@@ -3,47 +3,47 @@ import {connect} from "react-redux";
 
 class PageBudget extends Component {
 
-    // components
-    //  table component
-    //  row component
-    
-    render() {
-        return (
-            <div>
-                <table>
-                    <thead>
-                    <tr>
-                        <td>Category</td>
-                        <td>Amount Left</td>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>
-                            Rent
-                        </td>
-                        <td>
-                            $0
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Restaurants
-                        </td>
-                        <td>
-                            $40
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
+  // components
+  //  table component
+  //  row component
+
+  render() {
+    return (
+      <div>
+        <table>
+          <thead>
+          <tr>
+            <td>Category</td>
+            <td>Amount Left</td>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>
+              Rent
+            </td>
+            <td>
+              $0
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Restaurants
+            </td>
+            <td>
+              $40
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 
 }
 
 function mapStateToProps(state) {
-    return {}
+  return {}
 }
 
 export default connect(mapStateToProps, {})(PageBudget)
