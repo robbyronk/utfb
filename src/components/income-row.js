5ff87c5e91442e8e3fd8822fb14ccb332b4c3df2
robbyronk <robby.ronk@gmail.com>
2016-06-09 23:12:06 +0000

    Take in income and change function; handle date change

--- src/components/income-row.js
+++ src/components/income-row.js
@@ -2,11 +2,17 @@ import React, {Component, PropTypes} from "react";
 import MoneyInput from "../components/money-input";
 
 export default class IncomeRow extends Component {
+  handleDateChange(event) {
+    const changedIncome = Object.assign({}, this.props.income, {date: event.target.value})
+    this.props.change(changedIncome)
+  }
+
   render() {
     return (
       <tr>
         <td>
           <input value={this.props.income.date}
+                 onChange={this.handleDateChange.bind(this)}
                  type="text"/>
         </td>
         <td>
@@ -22,5 +28,6 @@ export default class IncomeRow extends Component {
 }
 
 IncomeRow.propTypes = {
-  income: PropTypes.object.isRequired
+  income: PropTypes.object.isRequired,
+  change: PropTypes.func.isRequired
 }
