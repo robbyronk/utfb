550a0bddda1f2077d3bc071cbf4f653dbe034c03
robbyronk <robby.ronk@gmail.com>
2016-06-09 23:07:22 +0000

    Add initial state and a update income reducer

--- src/reducers/incomes.js
+++ src/reducers/incomes.js
@@ -1,4 +1,20 @@
-function incomes(state = {}, action) {
+import update from 'react-addons-update'
+import { UPDATE_INCOME } from '../actions'
+
+const initialState = {
+  0: {
+    id: 0,
+    date: '2016-01-01',
+    payer: 'Acme Co',
+    amount: 100.05
+  }
+}
+
+function incomes(state = initialState, action) {
+  switch (action.type) {
+    case UPDATE_INCOME:
+      return update(state, {[action.payload.id]: {$set: action.payload}})
+  }
   return state
 }
 
