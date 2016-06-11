3796ac092a3cea90acec0920c2089a8b0e370d5a
robbyronk <robby.ronk@gmail.com>
2016-06-11 16:55:19 +0000

    Add category actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -43,3 +43,37 @@ export function updateBudget(budget) {
     payload: budget
   }
 }
+
+const defaultCategory = {
+  name: 'Yet to be named'
+}
+
+export const CREATE_CATEGORY = 'CREATE_CATEGORY'
+export function createCategory() {
+  const id = _.uniqueId('category')
+  return {
+    type: CREATE_CATEGORY,
+    payload: {
+      ...defaultCategory,
+      id
+    }
+  }
+}
+
+export const UPDATE_CATEGORY = 'UPDATE_CATEGORY'
+export function updateCategory(category) {
+  return {
+    type: UPDATE_CATEGORY,
+    payload: category
+  }
+}
+
+export const DELETE_CATEGORY = 'DELETE_CATEGORY'
+export function deleteCategory(id) {
+  return {
+    type: DELETE_CATEGORY,
+    payload: {
+      id
+    }
+  }
+}
