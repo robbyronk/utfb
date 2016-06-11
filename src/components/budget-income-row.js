207e096d2787c5ac24adddc4802446f515d87da2
robbyronk <robby.ronk@gmail.com>
2016-06-11 12:06:14 +0000

    Show and update budget from state

--- src/components/budget-income-row.js
+++ src/components/budget-income-row.js
@@ -2,16 +2,44 @@ import React, {Component, PropTypes} from "react";
 import MoneyInput from "../components/money-input";
 
 export default class BudgetIncomeRow extends Component {
+  handleCategoryChange(event) {
+    const changedIncome = Object.assign(
+      {},
+      this.props.budget,
+      {category: event.target.value}
+    )
+    this.props.change(changedIncome)
+  }
+
+  handleAmountChange(event) {
+    const changedIncome = Object.assign(
+      {},
+      this.props.budget,
+      {amount: event.target.value}
+    )
+    this.props.change(changedIncome)
+  }
+
   render() {
     return (
       <tr>
         <td>
-          <input type="text" defaultValue="aoeu"/>
+          <input onChange={this.handleCategoryChange.bind(this)}
+                 type="text"
+                 value={this.props.budget.category}
+          />
         </td>
         <td>
-          <MoneyInput/>
+          <MoneyInput amount={this.props.budget.amount}
+                      change={this.handleAmountChange.bind(this)}
+          />
         </td>
       </tr>
     )
   }
+}
+
+BudgetIncomeRow.propTypes = {
+  budget: PropTypes.object.isRequired,
+  change: PropTypes.func
 }
