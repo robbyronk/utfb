729c19a7c9a060e204c9dfcca1438224b38bfc81
robbyronk <robby.ronk@gmail.com>
2016-06-11 16:58:26 +0000

    Create category initial state and reducers

--- src/reducers/categories.js
+++ src/reducers/categories.js
@@ -1,4 +1,32 @@
-function categories(state = {}, action) {
+import update from 'react-addons-update'
+import { CREATE_CATEGORY, UPDATE_CATEGORY } from '../actions'
+
+const initialState = {
+  0: {
+    id: 0,
+    name: 'Uncategorized'
+  },
+  1: {
+    id: 1,
+    name: 'Rent'
+  },
+  2: {
+    id: 2,
+    name: 'Food'
+  },
+  3: {
+    id: 3,
+    name: 'Cats'
+  }
+}
+
+function categories(state = initialState, {type, payload}) {
+  switch (type) {
+    case CREATE_CATEGORY:
+      return Object.assign({}, state, {[payload.id]: payload})
+    case UPDATE_CATEGORY:
+      return update(state, {[payload.id]: {$set: payload}})
+  }
   return state
 }
 
