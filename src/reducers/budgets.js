d0715a6a60d201e66604db2cde25575e8f5ac73e
robbyronk <robby.ronk@gmail.com>
2016-06-11 12:04:16 +0000

    Create fake budgets state and update budgets reducer

--- src/reducers/budgets.js
+++ src/reducers/budgets.js
@@ -1,4 +1,24 @@
-function budgets(state = {}, action) {
+import update from 'react-addons-update'
+import { UPDATE_BUDGET } from '../actions'
+
+const initialState = {
+  0: {
+    id: 0,
+    category: 'Rent',
+    amount: 580
+  },
+  1: {
+    id: 1,
+    category: 'Food',
+    amount: 200
+  }
+}
+
+function budgets(state = initialState, {type, payload}) {
+  switch (type) {
+    case UPDATE_BUDGET:
+      return update(state, {[payload.id]: {$set: payload}})
+  }
   return state
 }
 
