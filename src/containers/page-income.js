2bbe12d31d75261f63e362d9f3a160bdef1844f6
robbyronk <robby.ronk@gmail.com>
2016-07-22 21:37:23 +0000

    Use actions that the horizon saga is listening for

--- src/containers/page-income.js
+++ src/containers/page-income.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
-import {newIncome, updateIncomeRequest, deleteIncome} from "../actions";
+import {createIncome, updateIncome, deleteIncome} from "../actions";
 import IncomeTable from "../components/income-table";
 import {browserHistory} from "react-router";
 
@@ -14,12 +14,12 @@ class PageIncome extends Component {
       <div>
         <h3>Paydays</h3>
         <button className="button expanded"
-                onClick={this.props.newIncome}>
+                onClick={this.props.createIncome}>
         Add an Income
         </button>
         <IncomeTable incomes={this.props.incomes}
                      deleteIncome={this.props.deleteIncome}
-                     updateIncome={this.props.updateIncomeRequest}/>
+                     updateIncome={this.props.updateIncome}/>
         <button className="success button expanded"
                 onClick={this.gotoCategorizeIncome}>Next
         </button>
@@ -36,7 +36,7 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps, {
-  newIncome,
+  createIncome,
   deleteIncome,
-  updateIncomeRequest
+  updateIncome
 })(PageIncome)
